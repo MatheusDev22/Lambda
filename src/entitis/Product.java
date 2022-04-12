f00f0e3b2cc21ca69c7123aa3fb5dfea2edd8c6b
MatheusDev22 <matheusbsb95@gmail.com>
2022-04-12 18:36:34 -0300

    However, this way our class is not closed to

--- src/entitis/Product.java
+++ src/entitis/Product.java
@@ -1,6 +1,6 @@
 package entitis;
 
-public class Product implements Comparable<Product>{
+public class Product {
 
 	private String name;
 	private Double price;
@@ -30,8 +30,5 @@ public class Product implements Comparable<Product>{
 	public String toString() {
 		return "Product [name = " + name + ", price = " + price + "]";
 	}
-	@Override
-	public int compareTo(Product p) {
-		return name.toUpperCase().compareTo(p.getName().toUpperCase() );// comparação desconsidenrando Maiusculo e Minusculo
-	}
+	
 }
